d7568a96282f6c8676775ac039943ed71cfa2687
zaryabdev <zaryabdev404@gmail.com>
2023-02-10 12:34:43 +0500

    added: LIC

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const chalk = require("chalk");
+
+const log = console.log;
+
+log(chalk.yellow.bold(`Not Zaryab`));
+log(chalk.gray.bold(`Github - https://github.com/zaryabdev`));
+log(
+    chalk.blue.bold(
+        `LinkedIn: https://www.linkedin.com/in/talha-zaryab-0b0162213/`
+    )
+);
+log(chalk.red.bold(`Email - zaraybdev404@gmail.com`));
+
+// function isEven(num) {
+//     log(chalk.blue.bold(num % 2 === 0));
+// }
+
+// function isOdd(num) {
+//     log(chalk.red.bold(num % 2 !== 0));
+// }
+
+// module.exports = { isEven, isOdd };
